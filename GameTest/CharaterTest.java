a65daa49fba78e8d5f00b3537ffa6971d0624453
ztSnai1 <ztsnailzju@163.com>
2022-02-17 01:45:50 +0800

    Add two basic package Weapon Charaters

---
+++ GameTest/CharaterTest.java
@@ -0,0 +1,28 @@
+package GameTest;
+
+import GameCharacters.Charaters;
+import Weapon.Weapon;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class CharaterTest {
+    public static void main(String[] args) {
+        ArrayList<String> rolelable = new ArrayList<>();
+        rolelable.add("中二");
+        rolelable.add("宅");
+        /*
+        Random rd = new Random();
+        for(int i =0;i<rd.nextInt(1,3);i++){
+        }
+         */
+        Charaters wyf = new Charaters(10,15,5,"Yufan Wu","I'm cooooooool",rolelable) {
+            @Override
+            public void actingcute() {
+                System.out.println("Hello! little game, i'm wyf, i'm coming!");
+            }
+        };
+        Weapon gun = new Weapon();
+        gun.Changwpattributes(wyf);
+    }
+}
